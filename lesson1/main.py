802b278fb3f5167f710b5dae70fa43dd90a6cc06
EnsarM15 <165320780+EnsarM15@users.noreply.github.com>
2026-04-02 20:46:21 +0200

    lesson1

---
+++ lesson1/main.py
@@ -0,0 +1,38 @@
+name="tini "
+surname="tini"
+age=77
+temperature=77.7
+
+full_name= name + surname
+
+users=["lisa","andi","edeni","lisa"]
+
+print(users)
+personiFituesi=users.pop()
+print(personiFituesi)
+
+# print(users)
+# users.insert(2,"donjeta")
+# print(users)
+
+# print(users.index("andi"))
+# print(users)
+# users.reverse
+
+# print(users[0])
+# print(users)
+#
+# users.append("roberti")
+# print(users)
+#
+# print(users.count("lisa"))
+#
+# users.clear()
+# print(users)
+
+# print(name)
+# print(age)
+# print(temperature)
+# print(full_name)
+#
+# print(type(name))
