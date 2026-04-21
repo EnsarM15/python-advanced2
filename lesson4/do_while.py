667ff09e1f6b64f3488976462593243a66ee2a72
EnsarM15 <165320780+EnsarM15@users.noreply.github.com>
2026-04-21 20:45:04 +0200

    ..

---
+++ lesson4/do_while.py
@@ -0,0 +1,6 @@
+while True:
+    number=1
+
+    number = int(input("shkruaj nje numer"))
+    if number > 0:
+     break
---
+++ lesson4/for_loop.py
@@ -0,0 +1,27 @@
+names = ["nil","nilBaba","nilKingi"]
+
+for name in  names:
+    print(name)
+
+shembulli = "digital school"
+for name in shembulli:
+    print(name)
+
+print("----------")
+
+for number in range (1,10):
+     print(number)
+
+for number in range(1, 10,2):
+ print(number)
+
+numbers = [12,15,30,95]
+
+maksimum=numbers[0]
+
+for num in numbers:
+    if num>maksimum:
+        maksimum=num
+
+print(maksimum)
+
