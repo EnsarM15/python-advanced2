f4e9d20509b3bc73e13598cbfbde944f2a228ffe
EnsarM15 <165320780+EnsarM15@users.noreply.github.com>
2026-05-05 20:44:08 +0200

    ..

---
+++ lesson8/kerri.py
@@ -0,0 +1,18 @@
+class Kerri :
+    def __init__(self,emri,viti,kilometrat,prejardhja):
+        self.emri=emri
+        self.viti = viti
+        self.kilometrat = kilometrat
+        self.prejardhja=prejardhja
+
+
+    def rriteShpejtsine(self):
+        print("shpejtesia e kerrit eshte duke u rritur")
+
+
+    def ndalu(self):
+        print("stooop")
+
+
+    def info(self):
+        print(f"{self.emri},eshte nje veture mjaft luksoze dhe eshte e prodhuar ne vitin:{self.viti},dhe eshte i prodhuar ne : {self.prejardhja}")
---
+++ lesson8/main.py
@@ -0,0 +1,15 @@
+from kerri import Kerri
+from personi import Personi
+
+kerri1 = Kerri("toyota","2020","00","japoni")
+
+
+kerri1.ndalu()
+
+print(kerri1.emri)
+
+kerri1.info()
+
+personi1 = Personi("ensari","2009","m")
+personi1.prezantimi()
+personi1.sayHi()
